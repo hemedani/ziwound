b1407dba72ee61fdecb5c300a9877ae292dd5828
Syd Amir <hemedani@gmail.com>
2026-05-20 19:31:16 +0330

    :sparkles: feat(back): Add photo relation support to country add function

--- back/src/country/add/add.fn.ts
+++ back/src/country/add/add.fn.ts
@@ -3,24 +3,24 @@ import { coreApp, country } from "../../../mod.ts";
 import type { MyContext } from "@lib";
 
 export const addFn: ActFn = async (body) => {
-	const { set, get } = body.details;
-	const { user }: MyContext = coreApp.contextFns
-		.getContextModel() as unknown as MyContext;
+  const { set, get } = body.details;
+  const { user }: MyContext = coreApp.contextFns
+    .getContextModel() as unknown as MyContext;
 
-	const { photoId, ...rest } = set;
+  const { photoId, ...rest } = set;
 
-	return await country.insertOne({
-		doc: rest,
-		relations: {
-			registrar: {
-				_ids: user._id,
-			},
-			...(photoId && {
-				photo: {
-					_ids: new ObjectId(photoId as string),
-				},
-			}),
-		},
-		projection: get,
-	});
+  return await country.insertOne({
+    doc: rest,
+    relations: {
+      registrar: {
+        _ids: user._id,
+      },
+      ...(photoId && {
+        photo: {
+          _ids: new ObjectId(photoId as string),
+        },
+      }),
+    },
+    projection: get,
+  });
 };
