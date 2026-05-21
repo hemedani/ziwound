4dbb2c159a2ef6fa88306bef7bcd039380961685
Syd Amir <hemedani@gmail.com>
2026-05-21 09:29:58 +0330

    :sparkles: feat(back): Add password hashing support to addUser function
    Hash the user's password using bcrypt before inserting into the database, extracting the password field from the set body to avoid storing it in plaintext.

--- back/src/user/addUser/addUser.fn.ts
+++ back/src/user/addUser/addUser.fn.ts
@@ -1,11 +1,12 @@
 import { type ActFn, ObjectId, type TInsertRelations } from "lesan";
 import { user } from "../../../mod.ts";
 import type { user_relations } from "@model";
+import { hash } from "@da/bcrypt";
 
 export const addUserFn: ActFn = async (body) => {
 	const { set, get } = body.details;
 
-	const { nationalCard, avatar, provinceId, cityId, ...rest } = set;
+	const { nationalCard, avatar, provinceId, cityId, password, ...rest } = set;
 
 	const relations: TInsertRelations<typeof user_relations> = {};
 
@@ -38,6 +39,7 @@ export const addUserFn: ActFn = async (body) => {
 	const addedUser = await user.insertOne({
 		doc: {
 			...rest,
+			password: password ? await hash(password as string) : undefined,
 			birth_date: rest.birth_date ? new Date(rest.birth_date as string) : undefined,
 		},
 		relations,
