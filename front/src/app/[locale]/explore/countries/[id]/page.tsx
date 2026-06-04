9ef949d169b396712049b3ebf5ea06e2c2b83423
Syd Amir <hemedani@gmail.com>
2026-06-05 01:21:13 +0330

    :bug: fix(ui): Add explicit grid-cols-1 and min-w-0 to prevent detail page layout overflow

--- front/src/app/[locale]/explore/countries/[id]/page.tsx
+++ front/src/app/[locale]/explore/countries/[id]/page.tsx
@@ -116,9 +116,9 @@ export default async function CountryDetailPage({ params }: CountryDetailPagePro
 
       {/* Content */}
       <div className="container mx-auto px-4 md:px-8 py-8 pb-20">
-        <div className="grid lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Main content */}
-          <div className="lg:col-span-2 space-y-8">
+          <div className="lg:col-span-2 min-w-0 space-y-8">
             {/* War Information */}
             <WarInfoSection
               fields={warFields}
